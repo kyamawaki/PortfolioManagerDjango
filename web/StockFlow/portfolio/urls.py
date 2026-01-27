2cd7661261e717f1c90cdb78fd7a53e9aa98a1b6
kyamawaki <yamawaki.mob@gmail.com>
2026-01-27 11:01:53 +0900

    clean: simplifing URLs

--- web/StockFlow/portfolio/urls.py
+++ web/StockFlow/portfolio/urls.py
@@ -7,8 +7,7 @@ urlpatterns = [
     path("assets/new/", views.asset_create, name="asset_create"),
     path("assets/<int:pk>/edit/", views.asset_edit, name="asset_edit"),
     path("assets/<int:pk>/delete/", views.asset_delete, name="asset_delete"),
-    #path("portfolio/", views.portfolio_list, name="portfolio_list"),
-    path("portfolio/holdings", views.holding_list, name="holdings_list"),
-    path("portfolio/add", views.add_holding, name="add_holding"),
+    path("holdings", views.holding_list, name="holdings_list"),
+    path("holdings/add", views.add_holding, name="add_holding"),
 ]
 
