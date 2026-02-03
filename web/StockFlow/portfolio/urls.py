a22a9b4bd73b8484ccf6743a5614b0e93661e50f
kyamawaki <yamawaki.mob@gmail.com>
2026-02-03 11:13:44 +0900

    feat Added update button.

--- web/StockFlow/portfolio/urls.py
+++ web/StockFlow/portfolio/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("holdings/create", views.holding_create, name="holding_create"),
     path("holdings/edit/<int:pk>", views.holding_edit, name="holding_edit"),
     path("holdings/delete/<int:pk>", views.holding_delete, name="holding_delete"),
+    path("holdings/update", views.holding_update, name="holding_update"),
 ]
 
