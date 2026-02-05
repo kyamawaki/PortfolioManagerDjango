647bfe40d118c4fdd39f2431cb20ff78552ab47f
kyamawaki <yamawaki.mob@gmail.com>
2026-02-05 12:57:52 +0900

    feat: Change API and added logging function
    yfinance is unstable so we changed the API
    API token is written in envfile(.env)

--- web/StockFlow/portfolio/services.py
+++ web/StockFlow/portfolio/services.py
@@ -1,17 +1,27 @@
-import yfinance as yf
+import requests
+import logging
+import os
 from datetime import date
+
+logger = logging.getLogger(__name__)
 
 ##############################################################
 # 最新の値（１日前の終値を取得する）
 ##############################################################
 def fetch_latest_price(ticker):
+    API_KEY = os.getenv("FINNHUB_API_KEY")
+    url =  f"https://finnhub.io/api/v1/quote?symbol={ticker}&token={API_KEY}"
     try:
-        data = yf.Ticker(ticker).history(period="1d")
-        if data.empty:
+        logger.info(f"[fetch_stock_price] ticker={ticker}")
+        data = requests.get(url, timeout=5).json()
+        if data is None:
             return None
-        return data["Close"].iloc[-1]
+        
+        logger.info(f"[fetched_stock_price]")
+        return data["c"]
 
     except Exception as e:
+        logger.exception(f"[fetch_stock_price] ERROR ticker={ticker}")
         return None
     
 ##############################################################
@@ -19,11 +29,16 @@ def fetch_latest_price(ticker):
 ##############################################################
 def fetch_usd_jpy():
     try:
-        data = yf.Ticker("USDJPY=X").history(period="1d")
-        if data is None or data.empty:
+        logger = logging.getLogger(__name__)
+        url = "https://api.frankfurter.app/latest?base=USD&symbols=JPY"
+        logger.info(f"[fetch_exchange_rate]")
+        data = requests.get(url, timeout=5).json()
+        if data is None:
             return None
     
-        return data["Close"].iloc[-1]
+        logger.info(f"[fetched_exchange_rate]")
+        return data["rates"]["JPY"]
 
     except Exception as e:
+        logger.exception(f"[fetch_exchange_rate] ERROR")
         return None
