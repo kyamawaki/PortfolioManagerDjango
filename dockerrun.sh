6db9d0ba2e82d85eb30146bb09f963a9ccb988b6
kyamawaki <yamawaki.mob@gmail.com>
2026-02-06 15:10:31 +0900

    chore: Set DNS to Docker Container

--- dockerrun.sh
+++ dockerrun.sh
@@ -4,11 +4,17 @@
 # --mount : bind the host's $(PWD)/web directory to /app in the container
 # -p : forward host port 3000 to container port 8000
 # start the container sleep infinity
- 
+
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+#echo $SCRIPT_DIR
+#exit 0
+
 # run docker container
 docker run -d --name django \
-  --env-file .env \
-  --mount type=bind,source=$(pwd)/web,target=/app/ \
+  --env-file $SCRIPT_DIR/.env \
+  --mount type=bind,source=$SCRIPT_DIR/web,target=/app/ \
+  --dns=8.8.8.8 \
+  --dns=8.8.4.4 \
   -p 3000:8000 \
   django sleep infinity
 #  django python manage.py runserver 0.0.0.0:8000
