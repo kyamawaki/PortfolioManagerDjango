c926041b05c5e207a92a9a9af5355ea4d52a82c4
kyamawaki <yamawaki.mob@gmail.com>
2026-02-06 17:57:11 +0900

    fix: Add a trailing slash to the URL pattern

--- web/StockFlow/portfolio/urls.py
+++ web/StockFlow/portfolio/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("assets", views.asset_list, name="asset_list"),
-    path("assets/create", views.asset_create, name="asset_create"),
-    path("assets/edit/<int:pk>", views.asset_edit, name="asset_edit"),
-    path("assets/delete/<int:pk>", views.asset_delete, name="asset_delete"),
-    path("assets/update", views.asset_update, name="asset_update"),
+    path("assets/", views.asset_list, name="asset_list"),
+    path("assets/create/", views.asset_create, name="asset_create"),
+    path("assets/edit/<int:pk>/", views.asset_edit, name="asset_edit"),
+    path("assets/delete/<int:pk>/", views.asset_delete, name="asset_delete"),
+    path("assets/update/", views.asset_update, name="asset_update"),
 ]
 
