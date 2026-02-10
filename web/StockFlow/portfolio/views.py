c9cb7d08963576c54e4657892eb5c8ee38e4d110
kyamawaki <yamawaki.mob@gmail.com>
2026-02-10 15:55:20 +0900

    chore Added comment

--- web/StockFlow/portfolio/views.py
+++ web/StockFlow/portfolio/views.py
@@ -108,11 +108,15 @@ def asset_update(request):
 
 def asset_summary_ticker(request):
     # 評価額 = current_price * quantity * exchange_rate
+    # F()を使用するとDB内で計算を実行する（ので高速）
+    # annotate()を使用するとAssetインスタンスにvalueという仮想フィールドが追加される
     assets = Asset.objects.annotate(
         value=F('current_price') * F('quantity') * F('exchange_rate')
     )
 
     # ticker ごとに集計
+    # assets.values('ticker)でtickerごとにレコードを取得
+    # assets.valueの合計をgroupd.total_valueに設定
     grouped = assets.values('ticker').annotate(
         total_value=Sum('value')
     )
@@ -121,6 +125,7 @@ def asset_summary_ticker(request):
     total = sum(item['total_value'] for item in grouped)
 
     # 割合を追加
+    # itemは辞書なので['ratio']が見つからない場合は辞書に追加される
     for item in grouped:
         item['ratio'] = (item['total_value'] / total * 100) if total > 0 else 0
 
