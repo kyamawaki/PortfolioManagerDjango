9a7f99e17bc7d61073ec563dbd0e2db6909a1ccb
kyamawaki <yamawaki.mob@gmail.com>
2026-04-08 09:16:33 +0900

    fixed: the ticker summary page was not displayed

--- web/StockFlow/portfolio/views/asset_summary_views.py
+++ web/StockFlow/portfolio/views/asset_summary_views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.db.models import Sum, F
+from django.db.models import Sum, F, Value, DecimalField
+from django.db.models.functions import Coalesce
 from portfolio.models import Asset
 
 # 資産クラスごとの円グラフ
@@ -45,20 +46,49 @@ def asset_summary_class(request):
     }
     return render(request, 'portfolio/asset_summary_class.html', context)
 
-
+# 銘柄ごとの集計
 def asset_summary_ticker(request):
+
+    # 銘柄ごとに集計(ticker, {dict})
+    grouped = {}
+    total_value = 0
+    assets = Asset.objects.all()
+    for a in assets:
+        grouped.setdefault(a.ticker, {"value":0})
+        grouped[a.ticker]["value"] += float(a.valuation_jpy)
+        total_value += float(a.valuation_jpy)
+
+    # 割合を追加
+    for ticker, data in grouped.items():
+        value = data["value"]
+        data["ratio"] = (value / total_value * 100) if total_value > 0 else 0
+
+    print(grouped)
+
+    # Chart.js用にラベルと値のリストを作る
+    labels = list(grouped.keys())
+    values = [data["value"] for data in grouped.values()]
+
+    return render(request, "portfolio/asset_summary_ticker.html", {
+        "grouped": grouped,
+        "total": total_value,
+        "labels": labels,
+        "values": values,
+    })
+
+'''
     # 評価額 = current_price * quantity * exchange_rate
     # F()を使用するとDB内で計算を実行する（ので高速）
     # annotate()を使用するとAssetインスタンスにvalueという仮想フィールドが追加される
     assets = Asset.objects.annotate(
-        value=F('current_price') * F('quantity') * F('exchange_rate')
+        value=F('current_price_jpy') * F('quantity') * F('exchange_rate')
     )
 
     # ticker ごとに集計
     # assets.values('ticker)でtickerごとにレコードを取得
     # assets.valueの合計をgroupd.total_valueに設定
     grouped = assets.values('ticker').annotate(
-        total_value=Sum('value')
+        total_value=Coalesce(Sum('value'), Value(0, output_field=DecimalField()))
     )
 
     # 全体の総額
@@ -73,4 +103,5 @@ def asset_summary_ticker(request):
         "grouped": grouped,
         "total": total,
     })
+'''
 
