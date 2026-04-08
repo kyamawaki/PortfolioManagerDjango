59c41da9c359b9c3af749f127ddb6d44838bd049
kyamawaki <yamawaki.mob@gmail.com>
2026-04-08 10:49:34 +0900

    feature: Display Asset name

--- web/StockFlow/portfolio/views/asset_summary_views.py
+++ web/StockFlow/portfolio/views/asset_summary_views.py
@@ -49,59 +49,51 @@ def asset_summary_class(request):
 # 銘柄ごとの集計
 def asset_summary_ticker(request):
 
-    # 銘柄ごとに集計(ticker, {dict})
-    grouped = {}
+    # 銘柄ごとに集計
+    grouped = {}    # {ticker: {name:"name", "value: 0, "class": "US_STOCK"}}
     total_value = 0
     assets = Asset.objects.all()
     for a in assets:
-        grouped.setdefault(a.ticker, {"value":0})
+        grouped.setdefault(a.ticker, {"name":"", "value":0, "class": a.asset_class})
         grouped[a.ticker]["value"] += float(a.valuation_jpy)
+        if not grouped[a.ticker].get("name"):
+            grouped[a.ticker]["name"] = a.name
         total_value += float(a.valuation_jpy)
 
     # 割合を追加
     for ticker, data in grouped.items():
         value = data["value"]
         data["ratio"] = (value / total_value * 100) if total_value > 0 else 0
+        # {ticker: {name: "name", value: 0, class: "US_STOCK", ratio:10}}
 
-    print(grouped)
+    # 資産クラスごとにソート
+    ORDER = [
+        'US_STOCK',
+        'US_BND',
+        'US_MMF',
+        'US_CASH',
+        'JP_STOCK',
+        'JP_FUND',
+        'JP_BND',
+        'JP_CASH',
+    ]
+    sorted_items = sorted(
+        grouped.items(),
+        key = lambda x: ORDER.index(x[1]["class"])
+    )
+    new_grouped = dict(sorted_items)
+    print(new_grouped)
 
     # Chart.js用にラベルと値のリストを作る
-    labels = list(grouped.keys())
-    values = [data["value"] for data in grouped.values()]
+    #labels = list(new_grouped.keys())
+    labels = [data["name"] for data in new_grouped.values()]
+    values = [data["value"] for data in new_grouped.values()]
 
     return render(request, "portfolio/asset_summary_ticker.html", {
-        "grouped": grouped,
+        "grouped": new_grouped,
         "total": total_value,
         "labels": labels,
         "values": values,
     })
 
-'''
-    # 評価額 = current_price * quantity * exchange_rate
-    # F()を使用するとDB内で計算を実行する（ので高速）
-    # annotate()を使用するとAssetインスタンスにvalueという仮想フィールドが追加される
-    assets = Asset.objects.annotate(
-        value=F('current_price_jpy') * F('quantity') * F('exchange_rate')
-    )
-
-    # ticker ごとに集計
-    # assets.values('ticker)でtickerごとにレコードを取得
-    # assets.valueの合計をgroupd.total_valueに設定
-    grouped = assets.values('ticker').annotate(
-        total_value=Coalesce(Sum('value'), Value(0, output_field=DecimalField()))
-    )
-
-    # 全体の総額
-    total = sum(item['total_value'] for item in grouped)
-
-    # 割合を追加
-    # itemは辞書なので['ratio']が見つからない場合は辞書に追加される
-    for item in grouped:
-        item['ratio'] = (item['total_value'] / total * 100) if total > 0 else 0
-
-    return render(request, "portfolio/asset_summary_ticker.html", {
-        "grouped": grouped,
-        "total": total,
-    })
-'''
 
